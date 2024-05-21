6fec98569b8a878768e94fdad773701a375e0615
michael <michaelgdimmitt@gmail.com>
2024-05-20 21:04:14 -0400

    hook it all up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Alert } from 'commet-example-site'
+// import { Alert } from '@trussworks/react-uswds'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +15,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -28,6 +32,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Alert type="success" heading="Success status" headingLevel="h4">
+          hello
+        </Alert>
     </>
   )
 }
