3f81c6b96bbbda813b65e6d412835424404fd9cf
michael <michaelgdimmitt@gmail.com>
2024-05-20 21:05:15 -0400

    revert back to trusworks
    This commit has been edited 1 times

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Alert } from 'commet-example-site'
-// import { Alert } from '@trussworks/react-uswds'
+// import { Alert } from 'commet-example-site'
+import { Alert } from '@trussworks/react-uswds'
 
 
 function App() {
